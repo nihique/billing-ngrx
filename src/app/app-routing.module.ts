8e192d65548479dc289696be96523a2707987ec7
Martin Kovářík <mk@Logis.local>
2017-05-13 18:07:25 +0200

    routing in templates between queues and task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QueuesComponent } from 'app/queues/queues.component';
+import { TaskComponent } from 'app/task/task.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    redirectTo: 'queues',
+    pathMatch: 'full'
+  },
+  {
+    path: 'queues',
+    component: QueuesComponent
+  },
+  {
+    path: 'task',
+    component: TaskComponent
   },
 ];
 
